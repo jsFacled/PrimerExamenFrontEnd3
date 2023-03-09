673257c56201203b2112b88b2f152ad54e3cddff
Facundo Ledezma <54073355+jsFacled@users.noreply.github.com>
2023-03-08 21:43:25 -0300

    creando componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 
 import './App.css'
+import Card from './components/Card'
+import Form from './components/Form'
 
 function App() {
 
   return (
     <div className="App">
+      <h1>Elije una mascota</h1>
+      <Form/>
+      <Card/>
 
       
       
